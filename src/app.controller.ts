b555bad8987a6c26ebe9492a4204c46836754cdb
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-01-06 12:06:52 -0300

    alteração controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("app")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // http://localhost:3000/app/hello
+  @Get("hello")
   getHello(): string {
     return this.appService.getHello();
   }
