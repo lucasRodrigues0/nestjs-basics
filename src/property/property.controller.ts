cdb84336a43b7bc48aebc1d3392d6fd4ceebbae3
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-01-06 13:51:55 -0300

    route params

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -6,5 +6,14 @@ export class PropertyController {
     @Get()
     findAll() {
         return "All properties";
+    }
+    @Get(':id')
+    findOne(@Param("id") id:string) {
+        return `finding id: ${id}`;
+    }
+
+    @Post()
+    create() {
+        return "this will create a property";
     }
 }
