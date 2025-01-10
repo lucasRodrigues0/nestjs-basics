836ed3c4b006080b32e8d7f5c7084428e56b6a13
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-01-10 15:20:46 -0300

    node_env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private configService: ConfigService) {}
 
   // http://localhost:3000/app/hello
   @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    return this.configService.get('NODE_ENV');
   }
 }
